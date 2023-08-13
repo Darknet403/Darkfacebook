0c8b6d7446f63b72ff80d2ef3d3bb3caee5e076f
DarkNet403 <142100190+Darknet403@users.noreply.github.com>
2023-08-13 07:11:56 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pkg install cowsay && pkg install php && pkg install nmap && pkg install traceroute && pkg install whois && pkg install git && pkg install bash && pkg install python && pkg install python-pip && pkg install curl && pkg install openssh && pkg install toilet && pkg install wget && pkg install figlet && pkg install ruby && pkg install nodejs && pkg install ffmpeg && pkg install neofetch && pkg install mc && pkg install nano && pkg install python2 && pkg install python3 && pkg install jq 
-pip install upgrade --pip && pip install requests colorama mechanize gem lolcat clone
+pkg update && pkg upgrade && apt-get update && apt-get upgrade && pkg install python-pip && pip install requests && pkg install cowsay && pkg install php && pkg install nmap && pkg install traceroute && pkg install whois && pkg install git && pkg install bash && pkg install python && pkg install curl && pkg install openssh && pkg install toilet && pkg install wget && pkg install figlet && pkg install ruby && pkg install nodejs && pkg install ffmpeg && pkg install neofetch && pkg install mc && pkg install nano && pkg install python2 && pkg install python3 && pkg install jq 
+pip install colorama mechanize gem lolcat clone
 gem install lolcat
-pip install -r requirements.txt 
+pip install -r requirements.txt
